b1567667ae45b9573954c16bf5ef9259dc22d9dd
Salustiano <salustianomeliann@gmail.com>
2025-05-29 19:36:52 -0300

    Se comenzo a trabajar con el archivo empleados

--- SisPersonal/Empleado.cs
+++ SisPersonal/Empleado.cs
@@ -1,5 +1,3 @@
-using Microsoft.VisualBasic;
-
 namespace EspacioEmpleado
 {
     public class Empleado
@@ -9,15 +7,43 @@ namespace EspacioEmpleado
         private DateTime fecha_de_nacimiento;
         private char estadoCivil;
         private DateTime fechaIngresoALaEmpresa;
-        private double sueldo;
-        enum Cargos {
+        private double sueldoBase;
+        enum Cargos
+        {
             Auxiliar,
             Administracion,
-            Ingeniero, 
-            Especialista, 
+            Ingeniero,
+            Especialista,
             Investigador
         }
         private Cargos cargo;
+
+        public int Edad(DateTime hoy)
+        {
+            return (hoy.Subtract(fecha_de_nacimiento).Days / 365); // obtengo los años  
+        }
+        public int antiguedad(DateTime hoy)
+        {
+            return (hoy.Subtract(fechaIngresoALaEmpresa).Days / 30);
+        }
+        public int Jubilacion(DateTime hoy)
+        {
+            int edad = Edad(hoy);
+            if (edad >= 65)
+            {
+                return 0;
+            }
+            else
+            {
+                return (65 - edad);
+            }
+        }
+        public int salario(int sueldoBase)
+        {
+            int antiguedad = antiguedad(hoy);
+            int  adicional = 0;
+        }
     }
+    
     
 }
